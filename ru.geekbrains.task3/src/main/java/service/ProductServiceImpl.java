8c7111dc7f85a414fd6b70e02d5b55025769f421
MF <ms.delo000@mail.ru>
2022-10-12 00:41:11 +0700

    Task3

---
+++ ru.geekbrains.task3/src/main/java/service/ProductServiceImpl.java
@@ -0,0 +1,40 @@
+package service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import persistence.Product;
+import persistence.ProductRepository;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<Product> getProductList() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public void saveOrUpdate(Product product) {
+        productRepository.saveOrUpdate(product);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+}
