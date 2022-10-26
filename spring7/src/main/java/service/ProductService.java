b5c09c3404a3a3470131fb4ceeed600facdbd38d
MF <ms.delo000@mail.ru>
2022-10-26 20:21:32 +0700

    Task7

---
+++ spring7/src/main/java/service/ProductService.java
@@ -0,0 +1,17 @@
+package service;
+
+import persistence.entities.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProductList();
+
+    void saveOrUpdate(Product product);
+
+    Product getProductById(Long id);
+
+    void deleteById(Long id);
+
+}
