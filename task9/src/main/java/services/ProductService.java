00c631b5f16dd1b3e4b03d1f5ac960c119d997e4
MF <ms.delo000@mail.ru>
2022-11-03 20:33:33 +0700

    Task9

---
+++ task9/src/main/java/services/ProductService.java
@@ -0,0 +1,48 @@
+package services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import entities.Product;
+import repositories.ProductRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@AllArgsConstructor
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Page<Product> findAllFilteredPaged(BigDecimal minPrice, BigDecimal maxPrice, String partTitle, Integer pageIndex, Integer productsPerPage) {
+        Pageable pageRequest = PageRequest.of(pageIndex - 1, productsPerPage);
+        Page<Product> productPage = productRepository.findProductsByPriceBetweenAndTitleLike(minPrice, maxPrice, "%"+partTitle+"%", pageRequest);
+        return productPage;
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveOrUpdate(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProductById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    public void deleteProductsInBatch(List<Product> productList) {
+        productRepository.deleteInBatch(productList);
+    }
+
+}
+
+
