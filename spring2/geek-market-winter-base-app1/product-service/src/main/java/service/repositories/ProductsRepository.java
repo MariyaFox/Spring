cb203bd00db00ebbe4792ee2346a398baca3d8a8
MF <ms.delo000@mail.ru>
2022-12-26 18:35:17 +0700

    Task 5 and 6

---
+++ spring2/geek-market-winter-base-app1/product-service/src/main/java/service/repositories/ProductsRepository.java
@@ -0,0 +1,7 @@
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.gb.cloud.project.product.service.model.Product;
+
+@Repository
+public interface ProductsRepository extends JpaRepository<Product, Long> {
+}
